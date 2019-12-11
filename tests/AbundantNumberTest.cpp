9962d270b7b5d8dbadb3087c5c8b02acd44f858d
Nelson Ruivo <nelsonruivo@gmail.com>
2019-12-11 19:42:14 +0000

    Added fail test cases for AbundantNumber.

--- tests/AbundantNumberTest.cpp
+++ tests/AbundantNumberTest.cpp
@@ -5,15 +5,11 @@ using namespace cppexercises;
 
 TEST(Numbers, IsAbundantNumber) {
   AbundantNumber abundantNumber;
-  for (auto& i : {
-      12, 18, 20, 24, 30, 36, 40, 42, 48, 54, 56, 60, 66, 70, 72, 78, 80, 84, 88, 90, 96, 100, 102, 104, 108, 112, 114, 120, 126, 132, 138, 140, 144, 150, 156, 160, 162, 168, 174, 176, 180, 186, 192, 196, 198, 200, 204, 208, 210, 216, 220, 222, 224, 228, 234, 240, 246, 252, 258, 260, 264, 270
-      }) {
-    EXPECT_EQ(true, abundantNumber.isAbundantNumber(i));
+  int abundantNumbers[] {12, 18, 20, 24, 30, 36, 40, 42, 48, 54, 56, 60, 66, 70, 72, 78, 80, 84, 88, 90, 96, 100, 102, 104, 108, 112, 114, 120, 126, 132, 138, 140, 144, 150, 156, 160, 162, 168, 174, 176, 180, 186, 192, 196, 198, 200, 204, 208, 210, 216, 220, 222, 224, 228, 234, 240, 246, 252, 258, 260, 264, 270};
+  int j=0;
+  for (auto& i : abundantNumbers) {
+    for (;j<=i;j++) {
+      EXPECT_EQ(j==i, abundantNumber.isAbundantNumber(j));
+    }
   }
-}
-
-TEST(Numbers, IsNotAbundantNumber) {
-  AbundantNumber abundantNumber;
-  for (int i=0; i<12; i++)
-    EXPECT_EQ(false, abundantNumber.isAbundantNumber(i));
 }
