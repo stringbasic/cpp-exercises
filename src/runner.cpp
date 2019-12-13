a3848d8fec28e164d50c1d7ae2a879f926803e8f
Nelson Ruivo <nelsonruivo@gmail.com>
2019-12-13 23:09:03 +0000

    Added options processor to runner.

--- src/runner.cpp
+++ src/runner.cpp
@@ -1,25 +1,60 @@
 #include <iostream>
 #include <string>
+#include "cxxopts.hpp"
 #include "UglyNumber.h"
+#include "AbundantNumber.h"
 
 using namespace std;
 using namespace cppexercises;
 
-int main()
+int main(int argc, char* argv[])
 {
-  int n,x=0;
-  cout << "\n\n Check whether a given number is an Ugly number:\n";
-  cout << "----------------------------------------------------\n";
-  cout << "Input an integer number: ";
-  cin >> n;
-  UglyNumber uglyNumber;
-  if (n <= 0) {
-    cout <<"Input a correct number.";
-  }
-  if (uglyNumber.isUglyNumber(n))
+  cxxopts::Options options("runner", "Run C++ exercises");
+  options.add_options()
+    ("uglyNumber", "checks if given number is ugly", cxxopts::value<int>())
+    ("isAbundantNumber", "checks if given number is abundante", cxxopts::value<int>())
+    ("listAbundantNumbers", "list abundant numbers until limit given", cxxopts::value<int>())
+    ("h,help", "print this help")
+    ;
+
+  if (argc == 1)
   {
-    cout << "It is an Ugly number."<<endl;
-  } else {
-    cout << "It is not an Ugly number."<<endl;
+    cout << options.help({""}) << endl;
+    exit(0);
+  }
+
+  auto result = options.parse(argc, argv);
+  if (result.count("help"))
+  {
+    cout << options.help({""}) << endl;
+    exit(0);
+  }
+
+  if (result.count("uglyNumber")) {
+    UglyNumber uglyNumber;
+    if (uglyNumber.isUglyNumber(result["uglyNumber"].as<int>()))
+    {
+      cout << "It is an Ugly number."<< endl;
+    } else {
+      cout << "It is not an Ugly number."<< endl;
+    }
+  }
+
+  if (result.count("isAbundantNumber")) {
+    AbundantNumber abundantNumber;
+    if (abundantNumber.isAbundantNumberRecursive(result["isAbundantNumber"].as<int>()))
+    {
+      cout << "It is an Abundant number."<< endl;
+    } else {
+      cout << "It is not an Abundant number."<< endl;
+    }
+  }
+
+  if (result.count("listAbundantNumbers")) {
+    AbundantNumber abundantNumber;
+    for (int i=0; i<result["listAbundantNumbers"].as<int>(); i++)
+        if (abundantNumber.isAbundantNumberRecursive(i))
+          cout << i << " ";
+    cout << endl;
   }
 }
